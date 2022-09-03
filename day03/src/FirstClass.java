09fbd95a8b18b11e2b3ba601bc3e86f92414f063
admin <admin@bzd.com>
2022-09-03 22:58:22 +0800

    FirstClass增加求偶数和

--- day03/src/FirstClass.java
+++ day03/src/FirstClass.java
@@ -1,9 +1,13 @@
 public class FirstClass {
     public static void main(String[] args) {
-        int sum = 0;
+        int sum = 0, sum1 = 0;
         for (int i = 0; i <= 100; i++) {
-                sum += i;
+            if (i % 2 == 0){
+                sum1 += i;
+            }
+            sum += i;
         }
+        System.out.println(sum1);
         System.out.println(sum);
     }
 }
